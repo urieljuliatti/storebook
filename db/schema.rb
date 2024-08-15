6cf6538175b5e4096d3f1cc96f40b681b3b4469b
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2024-08-15 08:54:17 -0300

    Adicionando scaffold de author

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_15_104250) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_15_115214) do
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
