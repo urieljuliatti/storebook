40627da5aecb8f11ad0460631ed65084b40ea2eb
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2024-08-15 10:28:00 -0300

    Adding devise

--- spec/requests/api/v1/books_spec.rb
+++ spec/requests/api/v1/books_spec.rb
@@ -12,21 +12,19 @@ require 'rails_helper'
 # of tools you can use to make these specs even more expressive, but we're
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
-RSpec.describe "/api/v1/books", type: :request do
+RSpec.describe '/api/v1/books', type: :request do
   # This should return the minimal set of attributes required to create a valid
   # Book. As you add validations to Book, be sure to
   # adjust the attributes here as well.
 
-  let(:author) {
-    @author = Author.create(name: 'Fulano')
-  }
   let(:valid_attributes) {
-    #skip("Add a hash of attributes valid for your model")
-    {"title"=>"foo", "body"=>"foo", "author_id" => author.id}
+    @author = Author.create(name: 'Fulano')
+    #skip('Add a hash of attributes valid for your model')
+    {'title'=>'foo', 'body'=>'foo', 'author_id' => @author.id }
   }
 
   let(:invalid_attributes) {
-    {"title"=>"", "body"=>"foo"}
+    {'title'=>'', 'body'=>'foo'}
   }
 
   # This should return the minimal set of values that should be in the headers
@@ -37,63 +35,63 @@ RSpec.describe "/api/v1/books", type: :request do
     {}
   }
 
-  describe "GET /index" do
-    it "renders a successful response" do
+  describe 'GET /index' do
+    it 'renders a successful response' do
       Book.create! valid_attributes
       get api_v1_books_url, headers: valid_headers, as: :json
       expect(response).to be_successful
     end
   end
 
-  describe "GET /show" do
-    it "renders a successful response" do
+  describe 'GET /show' do
+    it 'renders a successful response' do
       book = Book.create! valid_attributes
       get api_v1_book_url(book), as: :json
       expect(response).to be_successful
     end
   end
 
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Book" do
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Book' do
         expect {
           post api_v1_books_url,
                params: { book: valid_attributes }, headers: valid_headers, as: :json
         }.to change(Book, :count).by(1)
       end
 
-      it "renders a JSON response with the new book" do
+      it 'renders a JSON response with the new book' do
         post api_v1_books_url,
              params: { book: valid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:created)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
 
-    context "with invalid parameters" do
-      it "does not create a new Book" do
+    context 'with invalid parameters' do
+      it 'does not create a new Book' do
         expect {
           post api_v1_books_url,
                params: { book: invalid_attributes }, as: :json
         }.to change(Book, :count).by(0)
       end
 
-      it "renders a JSON response with errors for the new book" do
+      it 'renders a JSON response with errors for the new book' do
         post api_v1_books_url,
              params: { book: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
   end
 
-  describe "PATCH /update" do
-    context "with valid parameters" do
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
       let(:new_attributes) {
-        {"title"=>"foo 2", "body"=>"foo"}
+        {'title'=>'foo 2', 'body'=>'foo'}
       }
 
-      it "updates the requested book" do
+      it 'updates the requested book' do
         book = Book.create! valid_attributes
         patch api_v1_book_url(book),
               params: { book: new_attributes }, headers: valid_headers, as: :json
@@ -101,29 +99,29 @@ RSpec.describe "/api/v1/books", type: :request do
         expect(response).to have_http_status(:ok)
       end
 
-      it "renders a JSON response with the book" do
+      it 'renders a JSON response with the book' do
         book = Book.create! valid_attributes
         patch api_v1_book_url(book),
               params: { book: new_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:ok)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
 
-    context "with invalid parameters" do
-      it "renders a JSON response with errors for the book" do
+    context 'with invalid parameters' do
+      it 'renders a JSON response with errors for the book' do
         book = Book.create! valid_attributes
 
         patch api_v1_book_url(book),
               params: { book: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
   end
 
-  describe "DELETE /destroy" do
-    it "destroys the requested book" do
+  describe 'DELETE /destroy' do
+    it 'destroys the requested book' do
       book = Book.create! valid_attributes
       expect {
         delete api_v1_book_url(book), headers: valid_headers, as: :json
