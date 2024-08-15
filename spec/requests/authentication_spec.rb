e76d6a938a63e9ca1df36de592f0fdf1645c02c6
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2024-08-15 11:20:34 -0300

    Add test to authentication request

---
+++ spec/requests/authentication_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "/login", type: :request do
+
+  let(:user_params) {
+    {:email => "admin@email.com", :password => "123456"}
+  }
+
+  describe "GET /index" do
+    before(:each) do
+      @user = User.new user_params
+    end
+
+    it "renders a successful response" do
+      headers = { "ACCEPT" => "application/json" }
+
+      user = User.create! user_params
+      post "/login", :params => {:email => @user.email, :password => @user.password }, :headers => headers
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders an error response" do
+      headers = { "ACCEPT" => "application/json" }
+
+      post "/login", :params => {:email => @user.email, :password => "123" }, :headers => headers
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+end
