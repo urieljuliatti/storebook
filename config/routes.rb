b1abc94cf7dba9a7e89d61c4ad5dfe0c2d24ad74
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2024-08-16 08:11:27 -0300

    Adding book controller to show a book outside the admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users
 
   post 'login', to: 'authentication#login'
+  get '/books/:id', to: 'books#show', as: 'book'
 
   namespace :api do
     namespace :v1 do
