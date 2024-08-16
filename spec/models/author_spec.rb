fd66a20d0d15020d93752bccf8fc05c707841825
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2024-08-16 11:24:21 -0300

    Fixing tests

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  context 'validating presence of name' do
-    it 'expect to be valid' do
-      expect(Author.new(name: 'Uriel')).to be_valid
-    end
+
+  it "is valid with a name" do
+    author = Author.new(name: "J.K. Rowling")
+    expect(author).to be_valid
+  end
+
+  it "is invalid without a name" do
+    author = Author.new(name: nil)
+    expect(author).not_to be_valid
+    expect(author.errors[:name]).to include("can't be blank")
+  end
+
+  it "has many books" do
+    association = Author.reflect_on_association(:books)
+    expect(association.macro).to eq(:has_many)
   end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,12 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  context 'validating presence of' do
-    before(:each) do
-      @author = Author.create(name: 'Fulano')
-    end
-    it 'expect to be valid' do
-      expect(Book.create(title: 'Frankenstein', author_id: @author.id)).to be_valid
-    end
+
+  it "is valid with a title and an author" do
+    author = Author.create!(name: "J.K. Rowling")
+    book = Book.new(title: "Harry Potter and the Sorcerer's Stone", author: author)
+    expect(book).to be_valid
+  end
+
+  it "is invalid without a title" do
+    author = Author.create!(name: "J.K. Rowling")
+    book = Book.new(title: nil, author: author)
+    expect(book).not_to be_valid
+    expect(book.errors[:title]).to include("can't be blank")
+  end
+
+
+  it "is invalid without an author" do
+    book = Book.new(title: "Harry Potter and the Sorcerer's Stone")
+    expect(book).not_to be_valid
+    expect(book.errors[:author]).to include("must exist")
+  end
+
+  it "belongs to an author" do
+    association = Book.reflect_on_association(:author)
+    expect(association.macro).to eq(:belongs_to)
   end
 end
