1320d618c6e72e07a0c7105a5792cf1c72d6fc2d
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2024-08-17 05:16:53 -0300

    Adding resources to carts at the router

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   post 'login', to: 'authentication#login'
   resources :books, only: [:index, :show]
 
+  resources :carts, only: [:show, :create] do
+    post 'add_item/:book_id', to: 'carts#add_item', as: 'add_item'
+    delete 'remove_item/:book_id', to: 'carts#remove_item', as: 'remove_item'
+  end
+
   namespace :api do
     namespace :v1 do
       resources :books
